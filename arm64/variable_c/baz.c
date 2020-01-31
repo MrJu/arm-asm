2bcb4cf49261104398645eaf4ffba675ff2d2b23
andrew <mrju.email@gmail.com>
2020-01-31 21:35:28 +0800

    variable in c
    Signed-off-by: andrew <mrju.email@gmail.com>

---
+++ arm64/variable_c/baz.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2019 Andrew <mrju.email@gail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+
+int g0 = 0;
+int g1 = 0;
+int g2 = 2;
+int g3 = 3;
+int g4 = 4;
+static int g5 = 5;
---
+++ arm64/variable_c/qux.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2019 Andrew <mrju.email@gail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+
+int g0 = 0;
+int g1 = 1;
+static int g2 = 2;
+
+void func(void)
+{
+	int l0 = 0, l1 = 1, l2 = 2;
+	int l3 = 3, l4 = 4, l5 = 5;
+	static int l6 = 6;
+}
